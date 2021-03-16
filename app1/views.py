8cf907a14344bbb131ffdb2f6d88cc35df15a079
Daniel <danielryanelliott@gmail.com>
2021-03-15 22:32:45 -0400

    small changes

--- app1/views.py
+++ app1/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.shortcuts import render
-
 def app1(request):
     return render(request, 'app1.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
