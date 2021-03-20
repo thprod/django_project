f2b3198f70853503ed04bcb980e1993a4bc8ffc2
Taylor Hicks <taylorh075@gmail.com>
2021-03-20 16:41:44 -0700

    Updated server and added early form

--- app1/static/app1.js
+++ app1/static/app1.js
@@ -1,1 +1,18 @@
 
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+//const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const email = loginForm.username.value;
+    const pwd = loginForm.password.value;
+    const tOS - = loginForm.tOS.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
