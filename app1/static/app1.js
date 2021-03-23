6d1a8666e3f1a0d8963e76d47d0a619744d8f81d
Daniel <danielryanelliott@gmail.com>
2021-03-23 16:47:02 -0400

    -minor html/css changes

--- app1/static/app1.js
+++ app1/static/app1.js
@@ -1,1 +1,12 @@
+function changeNav() {
+  var nav = document.getElementById("mySidenav");
+  if (nav.style.width == '75vw') {
+    nav.style.width = '0vw';
+  } else {
+    nav.style.width = "75vw";
+  }
+}
 
+function myFunction(x) {
+  x.classList.toggle("change");
+}
